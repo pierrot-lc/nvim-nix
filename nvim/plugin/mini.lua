0bedee99238f1d88976422e6d0c03ba226a42453
Pierrot LC <pierrotlc@proton.me>
2024-01-28 19:47:32 +0100

    ♻️ : format and moved code to init.lua

--- nvim/plugin/mini.lua
+++ nvim/plugin/mini.lua
@@ -1,28 +1,28 @@
-require('mini.comment').setup {
-  mappings = {
-    comment = 'gc',
-    comment_line = 'gcc',
-    textobject = 'gc',
-  },
-}
+require("mini.comment").setup({
+	mappings = {
+		comment = "gc",
+		comment_line = "gcc",
+		textobject = "gc",
+	},
+})
 
-if pcall(require, 'which-key') then
-  require('which-key').register {
-    ['gc'] = { name = '+Comment' },
-  }
+if pcall(require, "which-key") then
+	require("which-key").register({
+		["gc"] = { name = "+Comment" },
+	})
 end
 
-require('mini.indentscope').setup()
+require("mini.indentscope").setup()
 
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'help', 'alpha', 'dashboard', 'NvimTree', 'Trouble', 'lazy', 'mason' },
-  callback = function()
-    vim.b.miniindentscope_disable = true
-  end,
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "help", "alpha", "dashboard", "NvimTree", "Trouble", "lazy", "mason" },
+	callback = function()
+		vim.b.miniindentscope_disable = true
+	end,
 })
 
 require("mini.splitjoin").setup({
-		mappings = {
-				toggle = "gS",
-		}
+	mappings = {
+		toggle = "gS",
+	},
 })
--- nvim/plugin/miscs.lua
+++ nvim/plugin/miscs.lua
@@ -1,124 +1,124 @@
-local spider = require('spider')
+local spider = require("spider")
 local spider_jump = function(move)
-  local jump_function = function()
-    spider.motion(move)
-  end
-  return jump_function
+	local jump_function = function()
+		spider.motion(move)
+	end
+	return jump_function
 end
-vim.keymap.set('n', 'w', spider_jump('w'), { desc = 'Spider-w' })
-vim.keymap.set('n', 'e', spider_jump('e'), { desc = 'Spider-e' })
-vim.keymap.set('n', 'b', spider_jump('b'), { desc = 'Spider-b' })
-vim.keymap.set('n', 'ge', spider_jump('ge'), { desc = 'Spider-ge' })
+vim.keymap.set("n", "w", spider_jump("w"), { desc = "Spider-w" })
+vim.keymap.set("n", "e", spider_jump("e"), { desc = "Spider-e" })
+vim.keymap.set("n", "b", spider_jump("b"), { desc = "Spider-b" })
+vim.keymap.set("n", "ge", spider_jump("ge"), { desc = "Spider-ge" })
 
-local augend = require('dial.augend')
-require('dial.config').augends:register_group {
-  default = {
-    augend.integer.alias.decimal,
-    augend.integer.alias.hex,
-    augend.date.alias['%Y/%m/%d'],
-    augend.constant.alias.alpha,
-    augend.constant.alias.Alpha,
-    augend.constant.alias.bool,
-    augend.constant.new { elements = { 'True', 'False' } },
-  },
-}
-vim.api.nvim_set_keymap('n', '<C-a>', require('dial.map').inc_normal(), { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-x>', require('dial.map').dec_normal(), { noremap = true })
+local augend = require("dial.augend")
+require("dial.config").augends:register_group({
+	default = {
+		augend.integer.alias.decimal,
+		augend.integer.alias.hex,
+		augend.date.alias["%Y/%m/%d"],
+		augend.constant.alias.alpha,
+		augend.constant.alias.Alpha,
+		augend.constant.alias.bool,
+		augend.constant.new({ elements = { "True", "False" } }),
+	},
+})
+vim.api.nvim_set_keymap("n", "<C-a>", require("dial.map").inc_normal(), { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-x>", require("dial.map").dec_normal(), { noremap = true })
 
-require('outline').setup {}
+require("outline").setup({})
 -- Use "!" operator to not change the window focus.
-vim.keymap.set('n', '<leader>lo', '<cmd>Outline!<cr>', { desc = 'Toggle symbols outline' })
+vim.keymap.set("n", "<leader>lo", "<cmd>Outline!<cr>", { desc = "Toggle symbols outline" })
 
-require('nvim-autopairs').setup()
+require("nvim-autopairs").setup()
 
 -- If you want insert `(` after select function or method item.
-local ok, cmp = pcall(require, 'cmp')
+local ok, cmp = pcall(require, "cmp")
 if ok then
-  local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-  cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+	local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+	cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 end
 
-require('conform').setup {
-  formatters_by_ft = {
-    lua = { 'stylua' },
-    python = { 'isort' },
-    sh = { 'shfmt' },
-    json = { 'jq' },
-    latex = { 'latexindent' },
-    tex = { 'latexindent' },
-  },
-}
-require('conform').formatters.latexindent = {
-  command = 'latexindent.pl',
-}
-
-vim.keymap.set('n', '<leader>F', require('conform').format, { desc = 'Format' })
-
-require('copilot').setup {
-  filetypes = {
-    ['*'] = true, -- Enable for all filetypes.
-  },
-  suggestion = {
-    enabled = true,
-    auto_trigger = false, -- Start suggesting as soon as you start typing.
-    keymap = {
-      accept = '<M-h>',
-      accept_word = '<M-l>',
-      accept_line = false,
-      next = '<M-k>',
-      prev = '<M-j>',
-      dismiss = false,
-    },
-  },
-  panel = {
-    enabled = false,
-  },
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "isort" },
+		sh = { "shfmt" },
+		json = { "jq" },
+		latex = { "latexindent" },
+		tex = { "latexindent" },
+	},
+})
+require("conform").formatters.latexindent = {
+	command = "latexindent.pl",
 }
 
-local leap_jump = function()
-  require('leap').leap { target_windows = { vim.fn.win_getid() } }
-end
+vim.keymap.set("n", "<leader>F", require("conform").format, { desc = "Format" })
 
-vim.keymap.set('n', '<leader>s', leap_jump, { desc = 'Leap jump' })
-
-require('neogit').setup()
-
-require('lint').linters_by_ft = {
-  markdown = { 'markdownlint', 'proselint' },
-  norg = { 'proselint' },
-  sh = { 'shellcheck' },
-  yaml = { 'yamllint' },
-}
-
-vim.diagnostic.config { update_in_insert = false }
-
-vim.api.nvim_create_autocmd({
-  'BufWritePost',
-  'BufReadPost',
-}, {
-  callback = function()
-    require('lint').try_lint()
-  end,
+require("copilot").setup({
+	filetypes = {
+		["*"] = true, -- Enable for all filetypes.
+	},
+	suggestion = {
+		enabled = true,
+		auto_trigger = false, -- Start suggesting as soon as you start typing.
+		keymap = {
+			accept = "<M-h>",
+			accept_word = "<M-l>",
+			accept_line = false,
+			next = "<M-k>",
+			prev = "<M-j>",
+			dismiss = false,
+		},
+	},
+	panel = {
+		enabled = false,
+	},
 })
 
-require('nvim-rooter').setup { manual = true }
+local leap_jump = function()
+	require("leap").leap({ target_windows = { vim.fn.win_getid() } })
+end
 
-require('nvim-surround').setup {}
+vim.keymap.set("n", "<leader>s", leap_jump, { desc = "Leap jump" })
 
-vim.opt.foldcolumn = '0'
+require("neogit").setup()
+
+require("lint").linters_by_ft = {
+	markdown = { "markdownlint", "proselint" },
+	norg = { "proselint" },
+	sh = { "shellcheck" },
+	yaml = { "yamllint" },
+}
+
+vim.diagnostic.config({ update_in_insert = false })
+
+vim.api.nvim_create_autocmd({
+	"BufWritePost",
+	"BufReadPost",
+}, {
+	callback = function()
+		require("lint").try_lint()
+	end,
+})
+
+require("nvim-rooter").setup({ manual = true })
+
+require("nvim-surround").setup({})
+
+vim.opt.foldcolumn = "0"
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
 vim.opt.foldenable = true
-require('ufo').setup {
-  provider_selector = function(bufnr, filetype, buftype)
-    return { 'treesitter', 'indent' }
-  end,
-}
+require("ufo").setup({
+	provider_selector = function(bufnr, filetype, buftype)
+		return { "treesitter", "indent" }
+	end,
+})
 
-require('todo-comments').setup()
+require("todo-comments").setup()
 
-require('toggleterm').setup {
-  open_mapping = '<C-g>',
-  direction = 'float',
-  shade_terminals = true,
-}
+require("toggleterm").setup({
+	open_mapping = "<C-g>",
+	direction = "float",
+	shade_terminals = true,
+})
