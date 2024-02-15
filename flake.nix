fc85e2127aef32fe5f2b295c4fd7366e6132117a
Pierrot LC <pierrotlc@proton.me>
2024-02-15 09:50:21 +0100

    👷 flake: add nix-community as extra-substituters

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   description = "Neovim derivation";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
