8fd24b4452c9ac16d36110599f71b529df07c820
Pierre Pereira <pierre.pereira@jolibrain.com>
2024-04-04 17:52:12 +0200

    wip: nvim module

--- nix/modules.nix
+++ nix/modules.nix
@@ -1,4 +1,17 @@
-{inputs, lib, config, ...}: {
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: let
+  package = import ./package.nix {
+    inherit pkgs;
+    inherit lib;
+    inherit config;
+    inherit inputs;
+  };
+in {
   options = {
     nvim-nix.theme = lib.mkOption {
       type = lib.types.str;
@@ -8,11 +21,7 @@
   };
 
   config = {
-    neovim-overlay = import ./neovim-overlay.nix {
-      inherit inputs;
-      config = {
-        theme = config.nvim-nix.theme;
-      };
-   };
+    # Add the package as an available package in `pkgs`.
+    pkgs = pkgs // {nvim-nix = package.nvim-pkg;};
   };
 }
--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -2,191 +2,22 @@
 {
   inputs,
   config,
-}: final: prev:
-with final.pkgs.lib; let
+}: final: prev: let
   pkgs = final;
 
-  pythonWithDebugpy = pkgs.python311.withPackages (ps: with ps; [debugpy]);
-
-  # The theme is selected based on the "config.theme" value.
-  # This set serves as a switch statement, where the vim command is called
-  # to load the theme.
-  themeCommands = {
-    "everforest" = "colorscheme everforest";
-    "catppuccin" = "colorscheme catppuccin-frappe";
+  package = import ./package.nix {
+    inherit pkgs;
+    inherit config;
+    inherit inputs;
+    lib = final;
   };
-
-  # Use this to create a plugin from a flake input
-  mkNvimPlugin = src: pname:
-    pkgs.vimUtils.buildVimPlugin {
-      inherit pname src;
-      version = src.lastModifiedDate;
-    };
-
-  # This is the helper function that builds the Neovim derivation.
-  mkNeovim = pkgs.callPackage ./mkNeovim.nix {};
-
-  all-plugins = with pkgs.vimPlugins; [
-    # Completers.
-    nvim-cmp
-    cmp-calc
-    cmp-nvim-lsp
-    cmp-path
-    cmp-rg
-    cmp_luasnip
-    (mkNvimPlugin inputs.gitmoji-nvim "gitmoji-nvim")
-
-    # DAPs.
-    nvim-dap
-    nvim-dap-ui
-    nvim-dap-virtual-text
-    {
-      plugin = nvim-dap-python;
-      config = ''
-        lua << EOF
-          require('dap-python').setup('${pythonWithDebugpy}/bin/python')
-        EOF
-      '';
-      type = "lua";
-    }
-
-    # Folds.
-    nvim-ufo
-
-    # Formatters.
-    conform-nvim
-
-    # Gits.
-    gitsigns-nvim
-    vim-fugitive
-
-    # Linters.
-    nvim-lint
-
-    # LSPs.
-    nvim-lspconfig
-    fidget-nvim
-    lsp_signature-nvim
-    neodev-nvim
-    outline-nvim
-
-    # Miscs.
-    copilot-lua
-    dial-nvim
-    nvim-autopairs
-    nvim-spider
-    todo-comments-nvim
-    toggleterm-nvim
-    (mkNvimPlugin inputs.nvim-puppeteer "nvim-puppeteer")
-    (mkNvimPlugin inputs.nvim-rooter "nvim-rooter")
-    (mkNvimPlugin inputs.vim-characterize "vim-characterize")
-    (mkNvimPlugin inputs.vim-kitty "vim-kitty")
-
-    # Minizinc.
-    (mkNvimPlugin inputs.vim-minizinc "vim-minizinc")
-
-    # Mini plugins.
-    mini-nvim
-
-    # Neorg.
-    neorg
-    diffview-nvim
-
-    # Snippets.
-    luasnip
-
-    # Themes.
-    catppuccin-nvim
-    (mkNvimPlugin inputs.everforest-nvim "everforest-nvim")
-
-    # Tree.
-    nvim-tree-lua
-
-    # Treesitter.
-    nvim-treesitter.withAllGrammars
-    nvim-treesitter-context
-    nvim-treesitter-refactor
-    nvim-treesitter-textobjects
-    hmts-nvim
-
-    # Telescope.
-    telescope-nvim
-    telescope-fzf-native-nvim
-    (mkNvimPlugin inputs.telescope-helpgrep-nvim "telescope-helpgrep-nvim")
-
-    # Tex.
-    vimtex
-    (mkNvimPlugin inputs.cmp-vimtex "cmp-vimtex")
-
-    # UI.
-    alpha-nvim
-    dressing-nvim
-    lualine-nvim
-    which-key-nvim
-    zen-mode-nvim
-
-    # General dependencies.
-    nvim-web-devicons
-    plenary-nvim
-  ];
-
-  # Extra packages for your plugins.
-  extraPackages = with pkgs; [
-    nodePackages_latest.nodejs
-    ripgrep
-
-    # DAPs.
-    pythonWithDebugpy
-
-    # Formatters.
-    alejandra
-    bibtex-tidy
-    jq
-    just
-    shfmt
-    stylua
-
-    # Linters.
-    nodePackages_latest.markdownlint-cli
-    proselint
-    shellcheck
-    yamllint
-
-    # LSPs.
-    fswatch # See https://github.com/neovim/neovim/pull/27347.
-    lua-language-server
-    marksman
-    nil
-    nodePackages_latest.bash-language-server
-    python311Packages.python-lsp-server
-    ruff-lsp
-    texlab
-
-    # LuaSnip dependencies.
-    luajitPackages.jsregexp
-  ];
 in {
   # This is the neovim derivation
   # returned by the overlay.
-  nvim-pkg = mkNeovim {
-    plugins = all-plugins;
-    inherit extraPackages;
-    extraLuaConfig = ''
-      -- Select the theme only after the config have been loaded.
-      vim.api.nvim_create_autocmd("VimEnter", {
-        group = vim.api.nvim_create_augroup("nvim_nix", { clear = true }),
-        desc = "Load theme",
-        command = '${themeCommands.${config.theme}}',
-      })
-    '';
-  };
+  nvim-pkg = package.nvim-pkg;
 
   # This can be symlinked in the devShell's shellHook.
-  nvim-luarc-json = final.mk-luarc-json {
-    nvim = final.neovim-nightly;
-    plugins = all-plugins;
-    neodev-types = "nightly";
-  };
+  nvim-luarc-json = package.nvim-luarc-json;
 
   # You can add as many derivations as you like.
   # Use `ignoreConfigRegexes` to filter out config
