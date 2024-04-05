08323e926a33d8c8b911a2f68f3e4925a4b85287
Pierrot LC <pierrotlc@proton.me>
2024-04-05 11:47:41 +0200

    feat(nvim-tree): side tree instead of floating window

--- nvim/plugin/nvim-tree.lua
+++ nvim/plugin/nvim-tree.lua
@@ -19,7 +19,7 @@ require("nvim-tree").setup({
 	},
 	view = {
 		float = {
-			enable = true,
+			enable = false,
 			quit_on_focus_loss = true,
 			open_win_config = { border = "rounded" }, -- See :h nvim_open_win.
 		},
