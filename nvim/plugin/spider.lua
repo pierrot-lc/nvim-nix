7cd19c62e544903287ce26da104654945b5e1267
Pierrot LC <pierrotlc@proton.me>
2024-04-15 17:57:42 +0200

    feat(moves): using same-line moves
    For normal and spider-moves as well.
    Inspired by the following plugin:
    https://github.com/yutkat/wb-only-current-line.nvim.

--- nvim/plugin/spider.lua
+++ nvim/plugin/spider.lua
@@ -1,11 +1,49 @@
 local spider = require("spider")
-local spider_jump = function(move)
+
+-- Same line jump.
+-- If the cursor moves to a different line, the cursor will be moved back to the original line.
+--
+-- @param direction string: The direction to move the cursor.
+-- @param move_fn function: The function to move the cursor.
+--
+-- NOTE: If the move is repeated using [count], the cursor will still be moved back to it's original position.
+local function same_line_jump(direction, move_fn)
+	local cursor_position = vim.api.nvim_win_get_cursor(0)
+
+	move_fn(direction)
+
+	local new_position = vim.api.nvim_win_get_cursor(0)
+	if cursor_position[1] ~= new_position[1] then
+		vim.api.nvim_win_set_cursor(0, { cursor_position[1], cursor_position[2] })
+	end
+end
+
+local function spider_jump(direction)
 	local jump_function = function()
-		spider.motion(move)
+		same_line_jump(direction, spider.motion)
 	end
 	return jump_function
 end
+
+local function normal_jump(direction)
+	local move_fn = function(direction_)
+		vim.cmd("normal! " .. direction_)
+	end
+
+	local jump_function = function()
+		same_line_jump(direction, move_fn)
+	end
+	return jump_function
+end
+
+-- Spider-moves.
 vim.keymap.set("n", "w", spider_jump("w"), { desc = "Spider-w" })
 vim.keymap.set("n", "e", spider_jump("e"), { desc = "Spider-e" })
 vim.keymap.set("n", "b", spider_jump("b"), { desc = "Spider-b" })
 vim.keymap.set("n", "ge", spider_jump("ge"), { desc = "Spider-ge" })
+
+-- Normal moves.
+vim.keymap.set("n", "W", normal_jump("W"), { desc = "Normal-W" })
+vim.keymap.set("n", "E", normal_jump("E"), { desc = "Normal-E" })
+vim.keymap.set("n", "B", normal_jump("B"), { desc = "Normal-B" })
+vim.keymap.set("n", "gE", normal_jump("gE"), { desc = "Normal-gE" })
