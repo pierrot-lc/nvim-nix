2904772c6fc65d22cf08fdff82c6cfaf69b23129
Pierre Pereira <pstmrtem@outlook.com>
2024-04-28 18:51:36 +0200

    fix: lualine rose pine dawn theme

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -3,7 +3,8 @@ local theme_parser = {
 	["everforest"] = "everforest",
 	["gruvbox"] = "gruvbox",
 	["kanagawa"] = "seoul256",
-	["rose-pine"] = "rose-pine",
+	["rose-pine"] = "rose-pine-alt",
+	["rose-pine-dawn"] = "rose-pine",
 }
 
 require("lualine").setup({
