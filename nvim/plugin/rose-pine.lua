e496c3f498af1fe6f9d038e274550247e8ec624d
Pierre Pereira <pstmrtem@outlook.com>
2024-04-28 18:43:25 +0200

    feat(themes): rose-pine dawn

--- nvim/plugin/rose-pine.lua
+++ nvim/plugin/rose-pine.lua
@@ -5,5 +5,9 @@ require("rose-pine").setup({
 })
 
 if vim.g.theme == "rose-pine" then
-	vim.cmd("colorscheme rose-pine-moon")
+	vim.cmd("colorscheme rose-pine-main")
+end
+
+if vim.g.theme == "rose-pine-dawn" then
+	vim.cmd("colorscheme rose-pine-dawn")
 end
