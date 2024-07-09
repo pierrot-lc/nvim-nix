8d93b1c1fce1d8f756298ea36804c88317ad0b5b
Pierrot LC <pierrotlc@proton.me>
2024-07-09 09:18:24 +0200

    feat(jump2d): better defaults

--- nvim/plugin/mini.jump2d.lua
+++ nvim/plugin/mini.jump2d.lua
@@ -1,1 +1,6 @@
-require("mini.jump2d").setup()
+local MiniJump2d = require("mini.jump2d")
+
+MiniJump2d.setup({
+	spotter = MiniJump2d.start(MiniJump2d.builtin_opts.word_start),
+	allowed_windows = { not_current = false },
+})
