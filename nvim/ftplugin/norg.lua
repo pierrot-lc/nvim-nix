fa7e03eb18c304c294fec66395188310edf35a3a
Pierrot LC <pierrotlc@proton.me>
2024-07-21 23:44:23 +0200

    fix: which-key v3

--- nvim/ftplugin/norg.lua
+++ nvim/ftplugin/norg.lua
@@ -9,11 +9,11 @@ vim.wo.wrap = false
 vim.keymap.set("n", "<LocalLeader>im", "<cmd>Neorg inject-metadata<CR>", { desc = "[neorg] Metadata", buffer = 0 })
 
 if pcall(require, "which-key") then
-	require("which-key").register({
-		["<LocalLeader>t"] = { name = "+TODO" },
-		["<LocalLeader>n"] = { name = "+Notes" },
-		["<LocalLeader>i"] = { name = "+Insert" },
-		["<LocalLeader>l"] = { name = "+List" },
-		["<LocalLeader>m"] = { name = "+Mode" },
-	}, { buffer = 0 })
+	require("which-key").add({
+		{ "<LocalLeader>t", group = "TODO", buffer = 0 },
+		{ "<LocalLeader>n", group = "Notes", buffer = 0 },
+		{ "<LocalLeader>i", group = "Insert", buffer = 0 },
+		{ "<LocalLeader>l", group = "List", buffer = 0 },
+		{ "<LocalLeader>m", group = "Mode", buffer = 0 },
+	})
 end
