3a9c16f26d8bacdbeae34ccffd15154c3af78c5b
Pierrot LC <pierrotlc@proton.me>
2024-11-03 17:55:25 +0100

    flake: do not overwrite neovim-nightly-overlay nixpkgs

--- flake.nix
+++ flake.nix
@@ -15,10 +15,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     gen-luarc.url = "github:mrcjkb/nix-gen-luarc-json";
-    neovim-nightly-overlay = {
-      url = "github:nix-community/neovim-nightly-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
 
     # Add bleeding-edge plugins here.
     # They can be updated with `nix flake update` (make sure to commit the
