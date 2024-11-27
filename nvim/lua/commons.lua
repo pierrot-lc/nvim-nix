112de63de8a8db9edfc716475a49b83ed51a7155
Pierrot LC <pierrotlc@proton.me>
2024-11-27 09:15:56 +0100

    fix: typst commons

--- nvim/lua/commons.lua
+++ nvim/lua/commons.lua
@@ -1,5 +1,5 @@
 -- ========================================================================== --
--- ==                            COMMON FILETYPES SETTINS                 == --
+-- ==                          COMMON FILETYPES SETTINS                    == --
 -- ========================================================================== --
 local augroup = vim.api.nvim_create_augroup("user_common", { clear = true })
 
@@ -15,7 +15,7 @@ vim.api.nvim_create_autocmd("FileType", {
 		"just",
 		"nix",
 		"toml",
-		"typ",
+		"typst",
 		"yaml",
 	},
 	callback = function()
