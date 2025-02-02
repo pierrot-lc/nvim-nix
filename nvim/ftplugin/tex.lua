9154cdf6892eafb8dfc784bdab38e9bfd84ca9b2
Pierrot LC <pierrotlc@proton.me>
2025-02-02 19:00:38 +0100

    ftplugins

--- nvim/ftplugin/tex.lua
+++ nvim/ftplugin/tex.lua
@@ -1,9 +1,4 @@
--- ========================================================================== --
--- ==                          LATEX SPECIFICS                             == --
--- ========================================================================== --
 vim.bo.expandtab = false
-vim.bo.shiftwidth = 2
-vim.bo.tabstop = 2
 vim.bo.textwidth = 100
 vim.wo.colorcolumn = ""
 vim.wo.conceallevel = 0
