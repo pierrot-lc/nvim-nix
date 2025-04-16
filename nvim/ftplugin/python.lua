fcc54c6f20d3de995589eedac38b39c9750fc00e
Pierrot LC <pierrotlc@proton.me>
2025-04-16 16:30:25 +0200

    feat(python): line-length of 100

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -1,4 +1,4 @@
-vim.bo.textwidth = 88
+vim.bo.textwidth = 100
 
 vim.keymap.set("n", "<LocalLeader>f", require("python-tools").puppeteer, { desc = "Puppeteer", buffer = 0 })
 vim.keymap.set("n", '<LocalLeader>"', require("python-tools").insert_docstrings, { desc = "Doc-string", buffer = 0 })
